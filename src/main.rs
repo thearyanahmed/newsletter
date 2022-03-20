d03841cb19adfb1fac38816dd7bcc40707b47ea4
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-20 21:43:01 +0600

    Fixed

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ use newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // run().await
     run().await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("failed to spawn app")
+    spawn_app().await.expect("failed to spawn app");
 
     let client = reqwest::Client::new();
 
@@ -10,7 +10,7 @@ async fn health_check_works() {
         .expect("failed to execute test");
 
     assert!(response.status().is_success());
-    assert!(Some(0),response.content_length());
+    assert_eq!(Some(0),response.content_length());
 }
 
 async fn spawn_app() -> std::io::Result<()> {

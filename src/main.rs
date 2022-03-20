9f787357c9251752709c526bf36d075047c87303
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-20 22:17:48 +0600

    Added a random port for tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use newsletter::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to port.");
+    //.expect("failed to bind.");
+
+    let port = listener.local_addr().unwrap().port();
+
+    println!("running on {}:{}","http://127.0.0.1".to_string(),port);
+
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,13 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
-    let response = client.get("http://localhost:8000/health_check")
+    let response = client
+        .get(format!("{}/health_check",&address))
         .send()
         .await
         .expect("failed to execute test");
@@ -13,8 +16,14 @@ async fn health_check_works() {
     assert_eq!(Some(0),response.content_length());
 }
 
-fn spawn_app() {
-    let server = newsletter::run().expect("failed to bind address.");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let server = newsletter::run(listener).expect("failed to bind address.");
 
     let _ = tokio::spawn(server);
+
+    return format!("http://127.0.0.1:{}",port);
 }

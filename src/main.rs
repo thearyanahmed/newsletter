be9e3c326fc4eb5e3c06fbfad298b4124bd347c4
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-22 01:13:42 +0600

    Added env logger

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use std::net::TcpListener;
 use newsletter::startup::run;
 use newsletter::configuration::get_configuration;
 use sqlx::PgPool;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_configuration().expect("failed to read configuration.");
 
     let address = format!("127.0.0.1:{}",config.application_port);

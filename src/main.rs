11209df87fd041f6ef604870c0358e95adecdeba
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-22 15:41:59 +0600

    Refactored subscriber

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer,JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt,EnvFilter,Registry};
 use tracing_log::LogTracer;
+use tracing::Subscriber;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("newsletter".into(),"info".into());
 
-    setup_logger();
+    init_subscriber(subscriber);
 
     let config = get_configuration().expect("failed to read configuration.");
 
@@ -27,12 +29,10 @@ async fn main() -> std::io::Result<()> {
     run(listener,connection_pool)?.await
 }
 
-fn setup_logger() {
-    LogTracer::init().expect("failed to set logger");
-
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
     // if RUST_LOG is absent, fallback to "info"
     let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
+        .unwrap_or_else(|_| EnvFilter::new(env_filter));
 
     let formatting_layer = BunyanFormattingLayer::new(
         "newsletter_dev".into(),
@@ -43,6 +43,10 @@ fn setup_logger() {
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer);
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("failed to set logger");
 
     set_global_default(subscriber).expect("failed to set subscriber");
 }

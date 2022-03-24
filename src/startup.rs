1edc7b8a2a127e790174314057e493b91108ff0d
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-24 16:23:16 +0600

    Added request id middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,16 @@
 use std::net::TcpListener;
 use actix_web::{HttpServer, web, App};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use crate::routes::{subscribe,health_check};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(connection_pool);
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check",web::get().to(health_check))
             .route("/subscriptions",web::post().to(subscribe))
             .app_data(db_pool.clone())

44fbed3ed367ca6b5ea8f7f50ed413db3060f551
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-28 04:02:30 +0600

    Updated config deployment

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,8 @@
 use secrecy::{Secret, ExposeSecret};
 use std::convert::{TryInto, TryFrom};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::ConnectOptions;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -25,6 +26,7 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 pub fn get_configuration() -> Result<Settings,config::ConfigError> {
@@ -82,14 +84,25 @@ impl TryFrom<String> for Environment {
 
 impl DatabaseSettings {
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(&self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        let mut options = self.without_db().database(&self.database_name);
+
+        options.log_statements(tracing::log::LevelFilter::Trace);
+        
+        options
     }
 }

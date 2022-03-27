42246997822a828cd3b95eb7e364bb0050dab75f
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-28 04:37:31 +0600

    Implemented AsRef<T> for SubscriberName

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
-use unicode_segmentation::UnicodeSegmentation;
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -19,38 +19,29 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    if is_invalid_name(&form.name) {
-        return HttpResponse::BadRequest().finish();
-    }
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name)
+    };
 
-    match insert_subscriber(&pool,&form).await {
+    match insert_subscriber(&pool,&new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish()
     }
 }
 
-pub fn is_invalid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-
-    let is_too_long = s.graphemes(true).count() > 256;
-
-    let forbidden_characters = ['/','(',')','"','<','>','\\','{','}','[',']'];
-    let contains_forbidden_characters = s.chars().any(|g|forbidden_characters.contains(&g));
-
-    ( is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
-}
 
 #[tracing::instrument(
     name = "saving new subscriber details in the database",
-    skip(pool,form)
+    skip(pool,new_subscriber)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(),sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(),sqlx::Error> {
     sqlx::query!(
             r#"
                 INSERT INTO subscriptions (id, email, name, subscribed_at)
                 VALUES ($1,$2,$3,$4)
             "#,
-            Uuid::new_v4(), form.email, form.name, Utc::now()
+            Uuid::new_v4(), new_subscriber.email, new_subscriber.name.as_ref(), Utc::now()
         )
         .execute(pool)
         .await

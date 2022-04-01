5dfb93cd0a441efc7e19321efdc74effe5daaf13
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-04-02 00:12:51 +0600

    Implemented TryFrom for NewSubscriber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,17 @@ pub struct FormData {
     email: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+
+        Ok(Self{ email, name })
+    }
+}
+
 #[tracing::instrument(
     name = "adding a new subscriber",
     skip(form,pool),
@@ -19,7 +30,7 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let new_subscriber = match parse_subscriber(form.0) {
+    let new_subscriber = match form.0.try_into() {
         Ok(sub) => sub,
         Err(_) => return HttpResponse::InternalServerError().finish() 
     };

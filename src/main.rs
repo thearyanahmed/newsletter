d412720a013cca152d6d5d03337e59b34ab7b526
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-04-03 01:00:53 +0600

    Added , extracted build up logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::net::TcpListener;
-use newsletter::email_client::EmailClient;
-use newsletter::startup::run;
+use newsletter::startup::{build};
 use newsletter::configuration::get_configuration;
 use newsletter::telemetry;
-use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -13,29 +10,9 @@ async fn main() -> std::io::Result<()> {
 
     let config = get_configuration().expect("failed to read configuration.");
 
-    let sender_email = config.email_client.sender()
-        .expect("invalid sender email address.");
+    let server = build(config).await?;
 
-    let timeout = config.email_client.timeout();
-
-    let email_client = EmailClient::new(
-        config.email_client.base_url,
-        sender_email,
-        config.email_client.authorization_token,
-        timeout
-    );
-
-    let address = format!("127.0.0.1:{}",&config.application.port);
-
-    let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(config.database.with_db());
-
-    let listener = TcpListener::bind(address.clone()).expect("failed to bind to port.");
-
-    println!("running on {}",address);
-
-    run(listener,connection_pool, email_client)?.await?;
+    server.await?;
 
     Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,38 @@
 use std::net::TcpListener;
 use actix_web::{HttpServer, web, App};
 use actix_web::dev::Server;
+use sqlx::postgres::PgPoolOptions;
+use crate::configuration::Settings;
 use crate::routes::{subscribe,health_check};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 use crate::email_client::{EmailClient};
+
+pub async fn build(config: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(config.database.with_db());
+    
+    let sender_email = config
+        .email_client
+        .sender()
+        .expect("invalid sender email address.");
+
+    let timeout = config.email_client.timeout();
+
+    let email_client = EmailClient::new(
+            config.email_client.base_url,
+            sender_email,
+            config.email_client.authorization_token,
+            timeout
+        );
+
+    let address = format!("127.0.0.1:{}",&config.application.port);
+
+    let listener = TcpListener::bind(address)?;
+
+    run(listener,connection_pool, email_client)
+}
 
 pub fn run(listener: TcpListener, connection_pool: PgPool, email_client: EmailClient) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(connection_pool);

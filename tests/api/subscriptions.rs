341f21fa87a67daccdb1d8529a55c5ddf8ed5319
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-04-04 21:53:49 +0600

    Fixed test suit, added test for db persistance

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -117,3 +117,29 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     assert_eq!(html_link,text_link);
 }
+
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let name = "aryan";
+    let email = "aryan@gmail.com";
+
+    let body = format!("name={}&email={}",name,email);
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscription(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("could not fetch data from db.");
+
+    assert_eq!(saved.email, email);
+    assert_eq!(saved.name, name);
+    assert_eq!(saved.status, "pending_confirmation");
+}
